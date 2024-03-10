7869476a971e5f0c72c60cb72603e3398042bccc
ngarciaotero <nasleygarciaotero@gmail.com>
2024-03-09 23:56:15 -0500

    Create new card w/ user's input from modal
    - Add event listener to the form submission to handle user input
    - Retrieve subject and image from the form
    - Create new card elements to hold the user input
    - Create an img elements with uploaded img and add to new card
    - Create a p element for the subject text and add to the new card
    - Add new card div to the collections content
    - Reset form and close modal after submission

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,7 @@ const img = document.getElementById("openModalButton");
 const modal = document.getElementsByClassName("create-collections-modal")[0];
 const span = document.getElementsByClassName("close")[0];
 const form = document.getElementsByClassName("create-modal-form")[0];
+const collectionContent = document.querySelector(".collection-content");
 
 function Library(name) {
   this.name = name;
@@ -31,4 +32,27 @@ span.addEventListener("click", () => {
     modal.style.display = "none";
     form.reset();
   }
+});
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const subject = form.querySelector("#subject").value;
+  const imageFile = form.querySelector("#image").files[0];
+
+  const collectionCard = document.createElement("div");
+  collectionCard.classList.add("collection-item");
+
+  const cardImage = document.createElement("img");
+  cardImage.src = URL.createObjectURL(imageFile);
+  collectionCard.appendChild(cardImage);
+
+  const cardSubject = document.createElement("p");
+  cardSubject.textContent = subject;
+  collectionCard.appendChild(cardSubject);
+
+  collectionContent.appendChild(collectionCard);
+
+  form.reset();
+  modal.style.display = "none";
 });
